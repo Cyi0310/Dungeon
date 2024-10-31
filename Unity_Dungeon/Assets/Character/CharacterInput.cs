02beead39931bb5c563dcc8ae344a11efca8da62
Cyi0310 <cpa11225637@gmail.com>
2024-10-31 14:29:47 +0800

    feat: 基本環境設定

---
+++ Unity_Dungeon/Assets/Character/CharacterInput.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInput : MonoBehaviour
+{
+    [SerializeField] private Character character;
+
+    void Start()
+    {
+        123
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
